8d9d9ecc0e4ebc69e322e8b8f05f0387c5f6114a
Soaib Momin <smomin20@gmail.com>
2017-04-07 23:38:03 -0400

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,223 @@
+﻿namespace HittingGame
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.labmissedtarget = new System.Windows.Forms.Label();
+            this.labtotaltarget = new System.Windows.Forms.Label();
+            this.labscore = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.panel2.Controls.Add(this.labmissedtarget);
+            this.panel2.Controls.Add(this.labtotaltarget);
+            this.panel2.Controls.Add(this.labscore);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Location = new System.Drawing.Point(23, 12);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(196, 100);
+            this.panel2.TabIndex = 0;
+            // 
+            // labmissedtarget
+            // 
+            this.labmissedtarget.AutoSize = true;
+            this.labmissedtarget.Location = new System.Drawing.Point(155, 50);
+            this.labmissedtarget.Name = "labmissedtarget";
+            this.labmissedtarget.Size = new System.Drawing.Size(13, 13);
+            this.labmissedtarget.TabIndex = 5;
+            this.labmissedtarget.Text = "0";
+            this.labmissedtarget.UseMnemonic = false;
+            this.labmissedtarget.Click += new System.EventHandler(this.labmissedtarget_Click);
+            // 
+            // labtotaltarget
+            // 
+            this.labtotaltarget.AutoSize = true;
+            this.labtotaltarget.Location = new System.Drawing.Point(155, 76);
+            this.labtotaltarget.Name = "labtotaltarget";
+            this.labtotaltarget.Size = new System.Drawing.Size(13, 13);
+            this.labtotaltarget.TabIndex = 4;
+            this.labtotaltarget.Text = "0";
+            // 
+            // labscore
+            // 
+            this.labscore.AutoSize = true;
+            this.labscore.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.labscore.Location = new System.Drawing.Point(155, 24);
+            this.labscore.Name = "labscore";
+            this.labscore.Size = new System.Drawing.Size(13, 13);
+            this.labscore.TabIndex = 3;
+            this.labscore.Text = "0";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Viner Hand ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label3.Location = new System.Drawing.Point(5, 68);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(136, 31);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Total Shots :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Viner Hand ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label2.Location = new System.Drawing.Point(3, 42);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(143, 31);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Missed Shots :";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Viner Hand ITC", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.AntiqueWhite;
+            this.label1.Location = new System.Drawing.Point(3, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(75, 31);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Score :";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 900;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button1.Location = new System.Drawing.Point(564, 62);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(91, 33);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Restart";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
+            this.button2.Location = new System.Drawing.Point(564, 20);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(91, 36);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Exit";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.pictureBox1.Image = global::HittingGame.Properties.Resources.mole2;
+            this.pictureBox1.Location = new System.Drawing.Point(12, 387);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(77, 60);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Viner Hand ITC", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.ForeColor = System.Drawing.Color.Orange;
+            this.label4.Location = new System.Drawing.Point(299, 20);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(0, 26);
+            this.label4.TabIndex = 5;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackgroundImage = global::HittingGame.Properties.Resources.back1;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(667, 551);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel2);
+            this.Cursor = System.Windows.Forms.Cursors.NoMove2D;
+            this.DoubleBuffered = true;
+            this.MaximumSize = new System.Drawing.Size(683, 590);
+            this.MinimumSize = new System.Drawing.Size(683, 590);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.Form1_MouseClick);
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label labscore;
+        private System.Windows.Forms.Label labmissedtarget;
+        private System.Windows.Forms.Label labtotaltarget;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HittingGame
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random();
+        int rand_x, rand_y;
+        int score = 0;
+        int totall_shoots = 0;
+        int missed_shoots = 0;
+        
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            rand_x = rand.Next(12,590);
+            rand_y = rand.Next(288,490);
+            pictureBox1.Location = new Point(rand_x, rand_y);
+
+            if (missed_shoots >= 10)
+            {
+                timer1.Stop();
+                label4.Text = "Game Over";
+                //labscore.Text = "";
+                //score = Convert.ToInt32("");
+            }
+
+        }
+
+        void gun_shoot_voice()
+        {
+            System.Media.SoundPlayer sp = new System.Media.SoundPlayer(@"C:\Windows\Media\gun-gunshot-01.wav");
+            sp.Play();
+        }
+
+        void fun_shoots()
+        {
+            gun_shoot_voice();
+            score++;
+            totall_shoots++;
+            labscore.Text = Convert.ToString(score);
+            labtotaltarget.Text = Convert.ToString(totall_shoots);
+        }
+
+        void fun_missed_shoots()
+        {
+            gun_shoot_voice();
+            missed_shoots++;
+            totall_shoots++;
+            labtotaltarget.Text = Convert.ToString(totall_shoots);
+            labmissedtarget.Text = Convert.ToString(missed_shoots);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            fun_shoots();
+        }
+
+        private void Form1_MouseClick(object sender, MouseEventArgs e)
+        {
+            fun_missed_shoots();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            timer1.Start();
+            label4.Text = "";
+            score = 0;
+            missed_shoots =0;
+            totall_shoots =0;
+            labscore.Text = Convert.ToString(score);
+            labtotaltarget.Text = Convert.ToString(totall_shoots);
+            labmissedtarget.Text = Convert.ToString(missed_shoots);
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void labmissedtarget_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
